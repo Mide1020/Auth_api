260f24e460623b5b4528da0e6d99ad78f2871032
mide1020r <akinrefonayomide1@gmai.com>
2025-12-31 10:19:41 +0100

    initial commit

--- database.py
+++ database.py
@@ -5,11 +5,16 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
-engine = create_engine(DATABASE_URL)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
 #SessionLocal = sessionmaker(bind=engine)
-
-
 
 SessionLocal = sessionmaker(
     autocommit=False,
